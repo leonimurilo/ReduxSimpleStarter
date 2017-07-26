aa70facc682dd15a9f81515eb52d2fe3417578e5
leonimurilo <leonimega@hotmail.com>
2017-07-25 22:32:37 -0300

    add learning comments

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,8 @@ import { combineReducers } from "redux";
 import BooksReducer from "./books"
 
 // consolidates reducers into the application state
+// the application state is composed by what is returned from each reducer.
+// reducers can return different things depending on the actions that happens during the execution of the app
 const rootReducer = combineReducers({
   books: BooksReducer
 });
